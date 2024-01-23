b57b49003b2279ce65710f3ae0a24c2b78460d33
Josh Schonstal <josh@schonstal.org>
2024-01-22 22:13:04 -0800

    Add initial projectile

---
+++ game/hazard/projectile/projectile.gd
@@ -0,0 +1,15 @@
+class_name Projectile
+extends Node2D
+
+@export var index:int = 0
+
+var velocity:Vector2
+
+func _ready() -> void:
+  velocity = Vector2(sin(index)*50, cos(index)*50);
+
+func _process(delta: float) -> void:
+  rotation += delta * TAU
+  
+func _physics_process(delta: float) -> void:
+  position += velocity * delta

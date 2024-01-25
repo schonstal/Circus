99feb1be527227740c79113498551af0af04d1de
Josh Schonstal <josh@schonstal.org>
2024-01-24 17:24:44 -0800

    More accurately sync to music

--- game/arena/circle.gd
+++ game/arena/circle.gd
@@ -12,6 +12,7 @@ func _ready() -> void:
 func _pump(m:int):
   var t = create_tween()
   t.tween_method(_update_radius, 0.9, 0.8, 0.1)
+  t.set_ease(Tween.EASE_IN)
   
 func _update_radius(r:float):
   self.radius = r
--- game/conductor/conductor.gd
+++ game/conductor/conductor.gd
@@ -5,19 +5,28 @@ extends Node
 @onready var audio_stream_player := $AudioStreamPlayer
 
 var beat := 0
-var time := 0.0
+var time_begin := 0.0
+var time_delay := 0.0
 
 func _ready() -> void:
-  var sync := AudioServer.get_time_to_next_mix()
-  await get_tree().create_timer(sync)
+  await get_tree().create_timer(3.).timeout
+  time_begin = Time.get_ticks_usec()
+  time_delay = AudioServer.get_time_to_next_mix() + AudioServer.get_output_latency()
   audio_stream_player.play()
 
 func _process(_delta: float) -> void:
-  var sync: float = AudioServer.get_time_to_next_mix() - AudioServer.get_output_latency()
-  var time: float = audio_stream_player.get_playback_position() + sync
+  if !audio_stream_player.playing:
+    return
+
+  var time = (Time.get_ticks_usec() - time_begin) / 1_000_000.0
+  time -= time_delay
+  if time < 0:
+    time = 0
+    return
   
-  var bps := (bpm * 0.25 / 60.0)
-  var current_beat := int(time / bps)
+  var bps:float = bpm / 60.0
+  var current_beat:int = floor(bps * time)
+  print(current_beat)
   
   if current_beat > beat:
     beat = current_beat

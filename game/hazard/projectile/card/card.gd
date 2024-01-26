b44e05bbf8b45a42bdba400d7635fde0a89da4b0
Josh Schonstal <josh@schonstal.org>
2024-01-25 22:09:32 -0800

    Add card projectile

---
+++ game/hazard/projectile/card/card.gd
@@ -0,0 +1,15 @@
+extends Projectile
+
+enum Suit {
+  CLUB,
+  DIAMOND,
+  HEART,
+  SPADE
+}
+
+@export var suit:Suit
+
+@onready var card: AnimatedSprite2D = $Card
+
+func _ready() -> void:
+  card.frame = suit

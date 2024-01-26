8e32b5c1cbc2076a023d3699e9a2d927b5be93f0
Josh Schonstal <josh@schonstal.org>
2024-01-25 20:50:37 -0800

    Update player to use key/gamepad controls

---
+++ game/autoload/input_service.gd
@@ -0,0 +1,4 @@
+extends Node
+
+func _ready() -> void:
+  Input.use_accumulated_input = true
--- game/player/player.gd
+++ game/player/player.gd
@@ -3,13 +3,18 @@ extends CharacterBody2D
 @export_range(1, 1000) var speed := 300
 
 func _process(delta: float) -> void:
-  var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-  #velocity = direction * speed
-  position = get_viewport().get_mouse_position()
+  #position = get_viewport().get_mouse_position()
+  calculate_velocity_from_joypad()
+  constrain_to_arena()
   
+func constrain_to_arena() -> void:
   var center := get_viewport_rect().size / 2.0
   if center.distance_to(position) > 400:
     position = center + (position - center).normalized() * 400
+  
+func calculate_velocity_from_joypad() -> void:
+  var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+  velocity = direction * speed
 
 func _physics_process(delta: float) -> void:
   move_and_slide()

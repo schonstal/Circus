1347f04b60f4da1794d00cd5a9071f7fabe44905
Josh Schonstal <josh@schonstal.org>
2024-01-26 21:07:09 -0800

    Implement quantization system in conductor

--- game/autoload/conductor/conductor.gd
+++ game/autoload/conductor/conductor.gd
@@ -3,18 +3,37 @@ extends Node
 @export var bpm := 146.0
 
 @onready var audio_stream_player := $AudioStreamPlayer
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-var beat := 0
+var note_types := {
+  4: quarter,
+  8: eighth,
+  12: twelfth,
+  16: sixteenth,
+  24: twenty_fourth,
+  32: thirty_second
+}
+
+var beats := {
+  4: 0,
+  8: 0,
+  12: 0,
+  16: 0,
+  24: 0,
+  32: 0
+}
+
 var time_begin := 0.0
 var time_delay := 0.0
 
 signal quarter(beat)
-signal thirty_second
-signal sixteenth
-signal eighth
-signal twelfth
-signal twenty_forth
+signal eighth(beat)
+signal twelfth(beat)
+signal sixteenth(beat)
+signal twenty_fourth(beat)
+signal thirty_second(beat)
+
+func _ready() -> void:
+  play_music()
 
 func play_music() -> void:
   time_begin = Time.get_ticks_usec()
@@ -31,9 +50,9 @@ func _process(_delta: float) -> void:
     time = 0
     return
   
-  var bps:float = bpm / 60.0
-  var current_beat:int = floor(bps * time)
-  
-  if current_beat > beat:
-    beat = current_beat
-    quarter.emit(beat)
+  for type in note_types.keys():
+    var bps:float = (type/4) * bpm / 60.0
+    var current_beat = floor(bps * time)
+    if current_beat > beats[type]:
+      beats[type] = current_beat
+      note_types[type].emit(int(beats[type]) % type)

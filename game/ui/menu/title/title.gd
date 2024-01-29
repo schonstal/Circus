9bf601006fe5a432b98762c0190c6ce957bd66e1
Josh Schonstal <josh@schonstal.org>
2024-01-28 17:18:28 -0800

    Add title screen transition

---
+++ game/ui/menu/title/title.gd
@@ -0,0 +1,11 @@
+extends Control
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var active := true
+
+func _process(delta: float) -> void:
+  if active && Input.is_action_just_pressed("focus"):
+    TransitionService.transition_out()
+    active = false
+    animation_player.play("start")
